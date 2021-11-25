bc7f6cb436ab49fc9c0c8758644a232876c39d3e
DavidC <adcc662@gmail.com>
2021-11-25 11:58:02 -0600

    Ambito Lexico con metodos privados

--- src/closure/closure.js
+++ src/closure/closure.js
@@ -8,14 +8,17 @@ moneyBox(10);
 
 const moneyBox = () => {
     var saveCoins = 0;
-    const countCoins = (coins) => {
+    const countCoins = coins => {
         saveCoins += coins;
         console.log(`MoneyBox: $${saveCoins}`);
-    }
-    return countCoins();
-}
+    };
+    return countCoins;
+};
 
 let myMoneyBox = moneyBox();
 myMoneyBox(4);
 myMoneyBox(6);
 myMoneyBox(10);
+
+
+
